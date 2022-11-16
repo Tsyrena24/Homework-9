aa4d1241e288a25e2626be567929cc0235c2705d
Tsiri <60728565+dnv0@users.noreply.github.com>
2022-11-16 23:21:30 +0700

    lines

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String phone = "985-547-12-58";
+        phone = phone.replace("-","");
+        phone = phone.replace(" ", "");
+        phone = phone.replace("+", "");
+        if (phone.length() == 10) {
+            phone = '7' + phone;
+        } else if (phone.length() > 11) {
+            throw new RuntimeException("Телефон слишком длинный");
+        } else if (phone.length() < 10) {
+            throw new RuntimeException("Телефон слишком короткий");
+        } else if (phone.length() == 11 && phone.charAt(0) != '7') {
+            throw new RuntimeException("Среди нас постороний");
+        }
+        System.out.println("phone = " + phone);
+        String expectedPhone = "79855471258";
+        if (phone.equals(expectedPhone)) {
+            System.out.println("Успех");
+        } else {
+            System.out.println("Неудача");
+        }
+        System.out.println();
+
+        //Задание 1
+        System.out.println("Задание 1");
+        String firstName = "ivan";
+        String middleName = "ivanovich";
+        String lastName = "ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+        System.out.println();
+
+        //Задание 2
+        System.out.println("Задание 2");
+        fullName = fullName.replace("i", "I");
+        System.out.println(fullName);
+        System.out.println();
+
+        //Задание 3
+        System.out.println("Задание 3");
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "e");
+        System.out.println(fullName);
     }
 }
